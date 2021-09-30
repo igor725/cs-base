fc24e23644f434b1ff55480bbc0181a1c43fc89e
igor725 <gvaldovigor@gmail.com>
2021-09-30 22:14:56 +0300

    Do not use List_Iter define

--- src/base_lists.c
+++ src/base_lists.c
@@ -9,11 +9,17 @@ cs_bool Base_LoadList(BList *list) {
 	if(!fp) return false;
 
 	cs_char buffer[64];
-	while(File_ReadLine(fp, buffer, 64))
+	while(File_ReadLine(fp, buffer, 64) > 0)
 		AList_AddField(&list->head, (cs_char *)String_AllocCopy(buffer));
 
 	File_Close(fp);
 	return true;
+}
+
+static cs_bool SaveIterator(AListField *ptr, AListField **head, void *ud) {
+	(void)head;
+	cs_str name = AList_GetValue(ptr).str;
+	return File_WriteFormat((cs_file)ud, "%s\n", name) > 0;
 }
 
 cs_bool Base_SaveList(BList *list) {
@@ -21,44 +27,41 @@ cs_bool Base_SaveList(BList *list) {
 	cs_file fp = File_Open(list->filename, "w");
 	if(!fp) return false;
 
-	cs_bool success = true;
-	AListField *ptr;
-	List_Iter(ptr, list->head) {
-		cs_str name = ptr->value.str;
-		cs_size len = String_Length(ptr->value.str);
-		if(File_WriteFormat(fp, "%s\n", name) != len) {
-			success = false;
-			break;
-		}
-	}
+	cs_bool success = AList_Iter(&list->head, fp, SaveIterator);
 
 	File_Close(fp);
 	return success;
 }
 
+static cs_bool ListChecker(AListField *ptr, AListField **head, void *ud) {
+	(void)head;
+	return !String_CaselessCompare((AList_GetValue(ptr).str), (cs_str)ud);
+}
+
 cs_bool Base_CheckList(BList *list, cs_str name) {
-	AListField *ptr;
-	List_Iter(ptr, list->head) {
-		if(String_CaselessCompare(ptr->value.str, name))
-			return true;
-	}
-	return false;
+	return !AList_Iter(&list->head, (void *)name, ListChecker);
 }
 
 cs_bool Base_AddList(BList *list, cs_str name) {
-	if(Base_CheckList(list, name)) return true;
+	if(Base_CheckList(list, name)) return false;
 	list->alerted = true;
 	return AList_AddField(&list->head, (cs_char *)String_AllocCopy(name)) != NULL;
 }
 
-cs_bool Base_RemoveList(BList *list, cs_str name) {
-	AListField *ptr;
-	List_Iter(ptr, list->head) {
-		if(String_CaselessCompare(ptr->value.str, name)) {
-			AList_Remove(&list->head, ptr);
-			list->alerted = true;
-			return true;
-		}
+static cs_bool ListRemover(AListField *ptr, AListField **head, void *ud) {
+	if(String_CaselessCompare((AList_GetValue(ptr).str), (cs_str)ud)) {
+		AList_Remove(head, ptr);
+		return false;
 	}
+
+	return true;
+}
+
+cs_bool Base_RemoveList(BList *list, cs_str name) {
+	if(!AList_Iter(&list->head, (void *)name, ListRemover)) {
+		list->alerted = true;
+		return true;
+	}
+
 	return false;
 }
